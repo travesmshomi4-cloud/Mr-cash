2530ad79c501b444505a1f6e3d1f0eb42f2b96d9
travesmshomi4-cloud <travesmshomi4@gmail.com>
2026-04-07 12:19:06 +0300

    Create CI workflow for Node.js in settingss.js
    Add GitHub Actions CI configuration for Node.js

--- settingss.js
+++ settingss.js
@@ -1,4 +1,36 @@
+name: Node.js CI
 
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   OWNER_NUMBER: "", // put your phone number here
